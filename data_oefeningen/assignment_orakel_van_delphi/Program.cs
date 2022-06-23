fa88919654bc733e0be9ff4b010073ea5af3ff1e
Elliot <elliotsmolders@gmail.com>
2022-06-24 01:20:55 +0200

    assignment orakel van delphi

---
+++ data_oefeningen/assignment_orakel_van_delphi/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Gebruik de random generator om een orakel/waarzegger te maken, namelijk de kleine broer of zus van het Orakel van Delphi. 
+Het programma zal aan de gebruiker vertellen hoe lang deze nog zal leven. Bijvoorbeeld: “Je zal nog 15 jaar leven.”.
+
+Het orakel zal enkel realistische getallen geven. M.a.w., getallen tussen de 5 en 125 jaar (onder de 5 zou grof zijn). */
+Random randomGenerator = new Random();
+int minLeeftijd = 5;
+int maxLeeftijd = 125;
+Console.WriteLine($"Je zal nog {randomGenerator.Next(minLeeftijd,maxLeeftijd)} jaar leven.");
