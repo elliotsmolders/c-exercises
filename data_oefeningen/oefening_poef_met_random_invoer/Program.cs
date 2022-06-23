c24dfa3b2c9a974569a73be4429040c0e4aba7ca
Elliot <elliotsmolders@gmail.com>
2022-06-24 01:26:45 +0200

    oefening poef met random invoer

---
+++ data_oefeningen/oefening_poef_met_random_invoer/Program.cs
@@ -0,0 +1,10 @@
+﻿Random randomGenerator = new Random();
+double som = 0;
+for(int i = 0; i<5;i++){
+    Console.WriteLine("Voer bedrag in?");
+    som += Math.Round(randomGenerator.NextDouble() * 20.0,2);
+    Console.WriteLine($"De poef staat op {som} euro.");
+}
+Console.WriteLine("*************************");
+double wekelijkseAfbetaling = 10;
+Console.WriteLine($"Het totaal van de poef is {som} euro en zal {((wekelijkseAfbetaling - som % wekelijkseAfbetaling)+som)/wekelijkseAfbetaling} weken duren ");
