e6330e1d7b1b7de34c9729f719414fea35e8997d
Elliot <elliotsmolders@gmail.com>
2022-06-25 15:37:41 +0200

    oefening grootste getal

---
+++ herhalingen_oefeningen/oefening_grootste_getal/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Vervolledig deze code zodat ook getoond wordt welke de grootste waarde is die werd ingevoerd. */
+
+int x= 0;
+int y= 0;
+int grootste = 0;
+do
+{
+    if(x>grootste){
+        grootste = x;
+    }
+    y = y + x;
+    Console.WriteLine("Voer gehele waarden in (32767=stop)");
+    string instring= Console.ReadLine();
+    x = Convert.ToInt32(instring);
+}while (x != 32767);
+Console.WriteLine($"Grootst ingegeven getal is: {grootste}");
+Console.WriteLine($"Som is {y}");
