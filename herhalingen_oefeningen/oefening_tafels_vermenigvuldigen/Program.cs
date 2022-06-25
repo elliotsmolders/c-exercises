e77b7fc532736feab02a6b920c41cdd243ff936f
Elliot <elliotsmolders@gmail.com>
2022-06-25 12:45:34 +0200

    oef tafels vermenigvuldigen

---
+++ herhalingen_oefeningen/oefening_tafels_vermenigvuldigen/Program.cs
@@ -0,0 +1,6 @@
+﻿/* Gebruik de kracht van loops om pijlsnel alle tafels van 1 tot en met 10 van vermenigvuldigen op het scherm te tonen 
+(dus van 1×1 tot 10×10 en alles daartussen). */
+int tafelVan = 10;
+for (int i = 1; i < 11; i++){
+    Console.WriteLine($"{i} x {tafelVan} = {i*tafelVan}");
+}
