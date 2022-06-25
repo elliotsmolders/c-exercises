53874d69d0858a129137679a1e87b3ae8aff4ec4
Elliot <elliotsmolders@gmail.com>
2022-06-25 14:58:48 +0200

    oefening for doordenker

---
+++ herhalingen_oefeningen/oefening_for_doordenker/Program.cs
@@ -0,0 +1,26 @@
+﻿/* [PRO] For doordenker
+Schrijf een programma dat de volgende output geeft, gegeven dat de gebruiker een maximum waarde invoert, dus als hij 4 ingeeft dan zal
+ de driehoek maximum 4 breed worden. Gebruik enkel for-loops! */
+ 
+ Console.WriteLine("geef breedte driehoek:");
+ int breedte = Convert.ToInt32(Console.ReadLine());
+
+ for (int i = 1; i < breedte; i++)
+ {
+    string starsToWrite = "";
+    for (int j = 0; j < i; j++)
+    {
+        starsToWrite += "*";
+    }
+    Console.WriteLine(starsToWrite);
+ }
+
+ for (int i = breedte; i >= 1; i--)
+ {
+    string starsToWrite = "";
+    for (int j = 0; j < i; j++)
+    {
+        starsToWrite += "*";
+    }
+        Console.WriteLine(starsToWrite);
+ }
