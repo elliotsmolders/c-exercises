0a0b4c4dfed69d20fe46b4f1002ee1b0d940f2bb
Elliot <elliotsmolders@gmail.com>
2022-06-26 12:36:27 +0200

    oefening become neo

---
+++ herhalingen_oefeningen/oefening_become_neo/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Volgende code genereert een beeld zoals dat ook in de cultfilm The Matrix plaatsvindt.
+Vul de code aan zodat de karakters random kleuren krijgen. Kan je het nog cooler maken? */
+
+Random rangen = new Random();
+Console.ForegroundColor = ConsoleColor.Green;
+while (true)
+{
+    //Genereer nieuw random teken:
+    char teken = Convert.ToChar(rangen.Next(62, 400));
+    //Zet teken op scherm
+    Console.Write(teken);
+    //Ietwat vertragen
+    System.Threading.Thread.Sleep(10); //dit getal is in milliseconden. Speel er gerust mee.
+    //random kleur
+    Console.ForegroundColor = (ConsoleColor)rangen.Next(1,14);
+}
