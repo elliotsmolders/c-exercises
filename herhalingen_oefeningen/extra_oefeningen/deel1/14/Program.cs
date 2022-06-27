c0455a69f288aedf47f3ae378ccb9d7ef466c781
Elliot <elliotsmolders@gmail.com>
2022-06-27 13:34:28 +0200

    oef 14

---
+++ herhalingen_oefeningen/extra_oefeningen/deel1/14/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Schrijf een programma dat controleert of een getal priem is of niet.
+ */
+ Console.WriteLine("geef een getal:");
+ int getal = Convert.ToInt32(Console.ReadLine());
+ bool isPriem=true;
+ for(int i = 2;i<getal;i++){
+    if(getal%i==0){
+        isPriem = false;
+        break;
+    }
+ }
+ Console.WriteLine($"{getal} {(isPriem?"is":"is niet")} priem");
