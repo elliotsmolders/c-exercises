cc370698eccd01485525fb8ae7fc11acaecc2d75
Elliot <elliotsmolders@gmail.com>
2022-06-27 14:36:52 +0200

    oef 6

---
+++ herhalingen_oefeningen/extra_oefeningen/deel2/6/Program.cs
@@ -0,0 +1,11 @@
+﻿//Vraag aan de gebruiker getallen tot hij -1 invoert. Toon het gemiddelde van de ingevoerde getallen.
+int som=0;
+int count = -1;
+int input=0;
+while(input!=-1){
+    count ++;
+    som += input;
+    Console.WriteLine("geef een getal, -1 om te stoppen");
+    input = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Som: {som}\naantal nummers: {count}\ngemiddelde: {(double)som/count:F2}");
