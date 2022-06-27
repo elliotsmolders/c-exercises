1a92f177e5f6034521ca3ab44b6d2d281bde077a
Elliot <elliotsmolders@gmail.com>
2022-06-27 16:21:04 +0200

    oef 2

---
+++ methoden_oefeningen/extra_oef_opwarmers/2/Program.cs
@@ -0,0 +1,5 @@
+﻿// Methode BerekenStraal die de straal van een cirkel kan berekenen waarvan je de diameter meegeeft (de diameter geef je mee als parameter).
+static double BerekenStraal(int diameter){
+    return diameter/2.0;
+}
+Console.WriteLine(BerekenStraal(3));
