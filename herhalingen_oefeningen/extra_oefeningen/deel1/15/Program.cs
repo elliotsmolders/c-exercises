7df75896fa3b8c45a0b5007d4aa3cf5c90a4bc81
Elliot <elliotsmolders@gmail.com>
2022-06-27 13:44:03 +0200

    oef 15

---
+++ herhalingen_oefeningen/extra_oefeningen/deel1/15/Program.cs
@@ -0,0 +1,42 @@
+﻿//Toon alle priemgetallen van 1 tot n.
+
+ Console.WriteLine("geef n:");
+ int n = Convert.ToInt32(Console.ReadLine());
+ bool isPriem;
+
+ for(int i = 2;i<=n;i++){
+    isPriem=true;
+    for (int j = 2; j < i; j++)
+    {
+        if(i%j==0){
+            isPriem=false;
+            break;
+        }
+    }
+    if(isPriem){
+        Console.WriteLine(i);
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+/* Schrijf een programma dat controleert of een getal priem is of niet.
+ */
+/*  Console.WriteLine("geef een getal:");
+ int getal = Convert.ToInt32(Console.ReadLine());
+ bool isPriem=true;
+ for(int i = 2;i<getal;i++){
+    if(getal%i==0){
+        isPriem = false;
+        break;
+    }
+ }
+ Console.WriteLine($"{getal} {(isPriem?"is":"is niet")} priem"); */
