86c6486df5649195df3e43e1ec571ab912d00427
Elliot <elliotsmolders@gmail.com>
2022-06-27 13:11:34 +0200

    oef 13

---
+++ herhalingen_oefeningen/extra_oefeningen/deel1/13/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Schrijf een programma een getal n ontbindt in factoren. Factoren zijn de getallen waardoor je n kan delen zonder rest (van bijvoorbeeld het
+ getal 100 zijn de factoren 1, 2, 4, 5, 10, 20, 25, 50 en 100).
+ */
+ Console.WriteLine("geef getal:");
+ int getal = Convert.ToInt32(Console.ReadLine());
+ for(int i= 1;i<=getal;i++){
+    if(getal%i==0){
+        Console.WriteLine(i);
+    }
+ }
