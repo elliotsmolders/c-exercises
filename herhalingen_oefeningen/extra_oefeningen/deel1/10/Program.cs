affab1cf954090439a2d2ebdd151c57f9d525c2d
Elliot <elliotsmolders@gmail.com>
2022-06-27 12:58:04 +0200

    oef 10

---
+++ herhalingen_oefeningen/extra_oefeningen/deel1/10/Program.cs
@@ -0,0 +1,7 @@
+﻿/* Schrijf een programma dat alle ascii karakters en hun waarde toont van 10 tot n (tip: char c = Convert.ToChar(65); zal hoofdletter
+ A tonen). */
+ Console.WriteLine("geef n:");
+ int n = Convert.ToInt32(Console.ReadLine());
+ for(int i =10;i <=n ;i++){
+    Console.WriteLine($"waarde :{i} karakter:{Convert.ToChar(i)}");
+ }
