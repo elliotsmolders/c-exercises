99716cdd581bf429c3bfa4cab92566b808cf5661
Elliot <elliotsmolders@gmail.com>
2022-06-27 16:28:17 +0200

    Revert "oef 1"
    This reverts commit 8615287803971374ebe5da1caa5c528d3ea69f48.

--- methoden_oefeningen/extra_oef_opwarmers/1/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// Methode Kwadraat die het kwadraat van een ingevoerd getal berekend ( het getal geef je mee als parameter).
-static int KwadraatBerekenen(int getal){
-    return getal*getal;
-}
-Console.WriteLine(KwadraatBerekenen(4));
