c81a92dcc276bac0c072b347097f8e35ab1d2452
Elliot <elliotsmolders@gmail.com>
2022-06-27 14:14:02 +0200

    oef 4

---
+++ herhalingen_oefeningen/extra_oefeningen/deel2/4/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Schrijf een programma om de eerste n termen van een harmonische reeks te tonen en bereken vervolgens de som van deze termen.
+ Als de gebruiker bijvoorbeeld 5 invoert verschijnt er (de laatste plus mag je tonen om geen onnodige ingewikkelde code te moeten schrijven)
+ :1/1 + 1/2 + 1/3 + 1/4 + 1/5 + Som = 2.283334 */
+
+ Console.WriteLine("geef n:");
+ int n = Convert.ToInt32(Console.ReadLine());
+ double som=0;
+ for(int i =1;i<=n;i++){
+    som += 1.0/i;
+    Console.Write($"1/{i} +");
+ }
+ Console.Write($"Som = {som}");
