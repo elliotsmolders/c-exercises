3f85a675d93b90822226a5043de14a5b20c3390f
Elliot <elliotsmolders@gmail.com>
2022-06-27 14:03:28 +0200

    oef 2

---
+++ herhalingen_oefeningen/extra_oefeningen/deel2/2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Toon alle machten tot 5 van n.
+Console.WriteLine("geef n:");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i<=5;i++){
+    Console.WriteLine($"{n} tot de macht {i} = {Math.Pow(n,i)}");
+}
