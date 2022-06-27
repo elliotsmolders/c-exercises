dc42003d9834e4cbc4d467800f5144b0db78f5d6
Elliot <elliotsmolders@gmail.com>
2022-06-27 13:08:12 +0200

    oef 12

---
+++ herhalingen_oefeningen/extra_oefeningen/deel1/12/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Schrijf een programma dat de macht van een getal toont. De gebruiker voert eerst het getal in, gevolgd door de macht 
+(bv. 2 en 4 zal als resultaat 16 geven (2 tot de 4e macht)). Merk op dat je geen gebruik mag maken van Math.Pow. Je dient zelf de
+ vermenigvuldiging helemaal(m.b.v. loops) uit te voeren. */
+ Console.WriteLine("geef getal in:");
+ int getal = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("geef exponent:");
+ int exponent = Convert.ToInt32(Console.ReadLine());
+ int result = 1;
+ for(int i = 1;i<=exponent;i++){
+    result *= getal;
+ }
+ Console.WriteLine($"{getal} tot de macht {exponent} is {result}");
+ 
