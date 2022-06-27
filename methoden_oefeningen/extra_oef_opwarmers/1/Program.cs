8615287803971374ebe5da1caa5c528d3ea69f48
Elliot <elliotsmolders@gmail.com>
2022-06-27 16:18:22 +0200

    oef 1

---
+++ methoden_oefeningen/extra_oef_opwarmers/1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Methode Kwadraat die het kwadraat van een ingevoerd getal berekend ( het getal geef je mee als parameter).
+static int KwadraatBerekenen(int getal){
+    return getal*getal;
+}
+Console.WriteLine(KwadraatBerekenen(4));
